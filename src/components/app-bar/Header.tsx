c8ba50a50ad3e3d2fac8aedc23f291c0e770922e
Justin-Arakaki <Justiniankoi@gmail.com>
2023-04-17 16:10:39 -0700

    added transparency to Header

--- src/components/app-bar/Header.tsx
+++ src/components/app-bar/Header.tsx
@@ -1,3 +1,4 @@
+import hexRgb from 'hex-rgb';
 import { AppBar, SxProps, Theme, Toolbar, Typography } from '@mui/material';
 import { useWindow } from '../../contexts/WindowContext';
 import CustyDrawer from './CustyDrawer';
@@ -15,21 +16,23 @@ export default function Header() {
   const appBarStyle: SxProps<Theme> = {
     position: 'fixed',
     color: 'text.primary',
-    bgcolor: 'background.default',
+    bgcolor: (theme: Theme) => {
+      const color = theme.palette.background.default;
+      return hexRgb(color, { alpha: 0.4, format: 'css' });
+    },
+    backdropFilter: 'blur(10px)',
   };
 
   return (
-    <>
-      <ElevationScroll>
-        <AppBar sx={appBarStyle}>
-          <Toolbar sx={{ justifyContent: 'space-between' }}>
-            <Typography variant="h3" color="primary">
-              just_
-            </Typography>
-            {navigationType}
-          </Toolbar>
-        </AppBar>
-      </ElevationScroll>
-    </>
+    <ElevationScroll>
+      <AppBar sx={appBarStyle}>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Typography variant="h3" color="primary">
+            just_
+          </Typography>
+          {navigationType}
+        </Toolbar>
+      </AppBar>
+    </ElevationScroll>
   );
 }
