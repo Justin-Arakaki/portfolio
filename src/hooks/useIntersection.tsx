68970a1a0dbe9b35b649f4860525dda0150b9e13
Justin-Arakaki <Justiniankoi@gmail.com>
2023-04-21 18:15:39 -0700

    created animation hooks

---
+++ src/hooks/useIntersection.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect, RefObject } from 'react';
+
+interface IntersectionOptionsProps {
+  root?: Element | null;
+  rootMargin?: string;
+  threshold?: number | number[];
+}
+
+export default function useIntersection(
+  ref: RefObject<Element>,
+  options: IntersectionOptionsProps
+) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsVisible(entry.isIntersecting);
+    }, options);
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref, options]);
+
+  return isVisible;
+}
